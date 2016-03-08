08757c416ebe5df25398a48e4a0cb97d20c84947
liuqinglin <liuqinglin@cd.tuan800.com>
2016-03-08 18:36:29 +0800

    add search

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,6 +7,7 @@ class Article < ActiveRecord::Base
     def query(params)
       result = self.all
       result = result.where(category_id: params[:category_id]) if params[:category_id].present?
+      result = result.where("title like ? OR body LIKE ?", "%#{params[:q]}%", "%#{params[:q]}%") if params[:q].present?
       result = result.offset(params[:offset].to_i) if params[:offset].present?
       result
     end
