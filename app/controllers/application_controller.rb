8a509f52515543f09ee7b12311e70f7c30905797
sven199109 <sven199109@gmail.com>
2016-03-11 22:00:22 +0800

    add i18n of japanese

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_locale
 
   def render_404
     render 'errors/404'
@@ -9,10 +10,14 @@ class ApplicationController < ActionController::Base
 
   def locale
     locale = params[:locale]
-    I18n.locale = locale.to_sym
+    session[:locale] = locale if locale
     redirect_to :back
-  rescue => e
-    p e
+  rescue => _
     redirect_to :back
+  end
+
+  protected
+  def set_locale
+    I18n.locale = session[:locale]
   end
 end
